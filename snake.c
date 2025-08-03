ca379f86fae274a2f2edafffcc01823794874e66
Kakte456 <komailali6644@gmail.com>
2025-08-03 21:44:08 +0000

    Added basic moving functionality to snake head but errorous: Grid overflow

--- snake.c
+++ snake.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,43 +12,68 @@ typedef struct node
     struct node *next;
 } node;
 
-#define GRID_SIZE 25
+#define COLUMNS 25
+#define ROWS 15
 
-bool GRID[GRID_SIZE][GRID_SIZE];
+bool GRID[ROWS][COLUMNS];
 
 // Prototypes
+void default_grid(void);
 void update_grid(node *head);
 void print_grid(void);
+void point_head(char arrow, node *head);
+void move_head(node *head);
 
 int main(void)
 {
-    // Default grid setup
-    for (int i = 0; i < GRID_SIZE; i++)
-    {
-        for (int j = 0; j < GRID_SIZE; j++)
-        {
-            GRID[i][j] = false;
-        }
-    }
-
     // Default head setup
     node *head = malloc(sizeof(node));
     if (head == NULL)
     {
         return 1;
     }
-    head->x = GRID_SIZE / 2;
-    head->y = GRID_SIZE / 2;
+    head->x = COLUMNS / 2;
+    head->y = ROWS / 2;
     head->axis = true;
     head->axis = true;
     head->next = NULL;
 
-    update_grid(head);
-    print_grid();
+    int i = 0;
+    while (i < 10)
+    {
+        // Default grid setup
+        default_grid();
+        // Updete grip with snake positions
+        update_grid(head);
+        // Print grid and snake
+        print_grid();
+
+        char cursor;
+        do
+        {
+            printf("(Up: R | Down: C | ->: F | <-: D) : ");
+            scanf("%c", &cursor);
+            cursor = toupper(cursor);
+        }
+        while (cursor != 'R' && cursor != 'C' && cursor != 'F' && cursor != 'D');
+
+        point_head(cursor, head);
+        move_head(head);
+        i++;
+    }
 
     return 0;
 }
-
+void default_grid(void)
+{
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            GRID[i][j] = false;
+        }
+    }
+}
 void update_grid(node *head)
 {
     for (node *ptr = head; ptr != NULL; ptr = ptr->next)
@@ -59,10 +85,10 @@ void update_grid(node *head)
 
 void print_grid(void)
 {
-    for (int i = 0; i < GRID_SIZE; i++)
+    for (int i = 0; i < ROWS; i++)
     {
         printf("|");
-        for (int j = 0; j < GRID_SIZE; j++)
+        for (int j = 0; j < COLUMNS; j++)
         {
             if (GRID[i][j] == true)
             {
@@ -74,5 +100,52 @@ void print_grid(void)
             }
         }
         printf("|\n");
+    }
+}
+
+void point_head(char arrow, node *head)
+{
+    if (arrow == 'R' || arrow == 'F')
+    {
+        head->direction = true;
+    }
+    else
+    {
+        head->direction = false;
+    }
+
+    if (arrow == 'R' || arrow == 'C')
+    {
+        head->axis = true;
+    }
+    else
+    {
+        head->axis = false;
+    }
+}
+
+void move_head(node *head)
+{
+    if (head->axis == true)
+    {
+        if (head->direction == true)
+        {
+            head->y++;
+        }
+        else
+        {
+            head->y--;
+        }
+    }
+    else
+    {
+        if (head->direction == true)
+        {
+            head->x++;
+        }
+        else
+        {
+            head->x--;
+        }
     }
 }
