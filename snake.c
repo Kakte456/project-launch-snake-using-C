3fa87926ce0bb2c4c7975c977b009dffe100d9e7
Kakte456 <komailali6644@gmail.com>
2025-08-03 12:50:31 +0000

    Default grid and snake setup + printing the first frame of grid

--- snake.c
+++ snake.c
@@ -1,7 +1,77 @@
+#include <stdbool.h>
 #include <stdio.h>
+
+typedef struct node
+{
+    int x;
+    int y;
+    bool axis;
+    bool direction;
+    struct node *next;
+} node;
+
+const int GRID_SIZE = 25;
+
+bool GRID[GRID_SIZE][GRID_SIZE];
+
+// Prototypes
+void update_grid(node *head);
+void print_grid(void);
 
 int main(void)
 {
-    printf("Hello github! lets make snake!\n");
+    // Default grid setup
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            GRID[i][j] = false;
+        }
+    }
+
+    // Default head setup
+    node *head = malloc(sizeof(node));
+    if (head == NULL)
+    {
+        return 1;
+    }
+    head->x = GRID_SIZE / 2;
+    head->y = GRID_SIZE / 2;
+    head->axis = true;
+    head->axis = true;
+    head->next = NULL;
+
+    update_grid(head);
+    print_grid();
+
     return 0;
+}
+
+void update_grid(node *head)
+{
+    for (node *ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        GRID[ptr->y][ptr->x] = true;
+    }
+    return;
+}
+
+void print_grid(void)
+{
+    for (int i = 0; i < GRID_SIZE; i++)
+    {
+        printf("|");
+        for (int j = 0; j < GRID_SIZE; j++)
+        {
+            if (GRID[i][j] == true)
+            {
+                printf("o");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("|\n");
+    }
 }
