8d757a42bf30b1d4676f36a2a3f02d6dc8482702
Kakte456 <komailali6644@gmail.com>
2025-08-04 17:44:24 +0000

    Testing game / grid with time progression

---
+++ new.c
@@ -0,0 +1,196 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct node
+{
+    int x;
+    int y;
+    bool axis;
+    bool direction;
+    struct node *next;
+} node;
+
+#define COLUMNS 25
+#define ROWS 15
+
+bool GRID[ROWS][COLUMNS];
+
+// Prototypes
+void default_grid(void);
+void update_grid(node *head);
+bool print_grid(void);
+void point_head(char arrow, node *head);
+void move_head(node *head);
+bool crash(void);
+
+int main(void)
+{
+    // Default head setup
+    node *head = malloc(sizeof(node));
+    if (head == NULL)
+    {
+        return 1;
+    }
+    head->x = COLUMNS / 2;
+    head->y = ROWS / 2;
+    head->direction = true;
+    head->axis = false;
+    head->next = NULL;
+
+    long before = time(NULL);
+    long after;
+    while (true)
+    {
+        // Default grid setup
+        default_grid();
+        // Updete grid with snake positions
+        update_grid(head);
+        // Print grid and snake
+        if (!print_grid())
+        {
+            free(head);
+            return 1;
+        }
+
+        move_head(head);
+
+        if (head->x < 0 || head->x >= COLUMNS || head->y < 0 || head->y >= ROWS)
+        {
+            if (!crash())
+            {
+                free(head);
+                return 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        
+        do
+        {
+            after = time(NULL);
+        }
+        while (after < before + 1);
+
+        before = after;
+    }
+
+    free(head);
+    return 0;
+}
+
+void default_grid(void)
+{
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            GRID[i][j] = false;
+        }
+    }
+}
+
+void update_grid(node *head)
+{
+    for (node *ptr = head; ptr != NULL; ptr = ptr->next)
+    {
+        GRID[ptr->y][ptr->x] = true;
+    }
+    return;
+}
+
+bool print_grid(void)
+{
+    FILE *screen = fopen("screen.txt", "w");
+    if (screen == NULL)
+    {
+        // Error protocols
+        return false;
+    }
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        fprintf(screen, "|");
+        for (int j = 0; j < COLUMNS; j++)
+        {
+            if (GRID[i][j] == true)
+            {
+                fprintf(screen, "O");
+            }
+            else
+            {
+                fprintf(screen, " ");
+            }
+        }
+        fprintf(screen, "|\n");
+    }
+
+    fclose(screen);
+    return true;
+}
+
+void point_head(char arrow, node *head)
+{
+    if (arrow == 'C' || arrow == 'F')
+    {
+        head->direction = true;
+    }
+    else
+    {
+        head->direction = false;
+    }
+
+    if (arrow == 'R' || arrow == 'C')
+    {
+        head->axis = true;
+    }
+    else
+    {
+        head->axis = false;
+    }
+    return;
+}
+
+void move_head(node *head)
+{
+    if (head->axis == true)
+    {
+        if (head->direction == true)
+        {
+            head->y++;
+        }
+        else
+        {
+            head->y--;
+        }
+    }
+    else
+    {
+        if (head->direction == true)
+        {
+            head->x++;
+        }
+        else
+        {
+            head->x--;
+        }
+    }
+    return;
+}
+
+bool crash(void)
+{
+    FILE *screen = fopen("screen.txt", "a");
+    if (screen == NULL)
+    {
+        return false;
+    }
+
+    fprintf(screen, "Game over!!\n");
+    fclose(screen);
+    return true;
+}
